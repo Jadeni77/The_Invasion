fe1b8ac15d02460a2a0595c3295cf8fd24804332
XiaobinMei <mei.xiaob@northeastern.edu>
2025-07-08 22:27:54 -0400

    use the react built in context

--- src/game/GameContext.jsx
+++ src/game/GameContext.jsx
@@ -1,1 +1,24 @@
 //This file handles Game interaction with the GameEngine.js
+
+import React from "react";
+import { GameEngine } from "./GameEngine";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+} from "react";
+
+//create game context (React built-in CreateContect())
+export const GameContext = createContext();
+
+export const useGame = () => {
+  return useContext(GameContext);
+};
+
+//Game provider Component
+export const GameProvider = ({ children }) => {
+    
+};
