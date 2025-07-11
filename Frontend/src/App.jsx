62a6e26761acce87b8a92830eeef2576f577cff6
XiaobinMei <mei.xiaob@northeastern.edu>
2025-07-11 17:41:38 -0400

    impleent the GameBoard logic on how to handle different actions, check out the comment above the function for more information. <GameProvider> and other are added to App.jsx but it will not showing anything yet

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import './App.css';
+import "./App.css";
+import { GameProvider } from "./component/GameLogic (MVC)/GameContext";
+import GameBoard from "./component/GameRendering/GameBoard";
 
 function App() {
-
+  return (
+    <GameProvider>
+      <GameBoard />
+    </GameProvider>
+  );
 }
 
 export default App;
